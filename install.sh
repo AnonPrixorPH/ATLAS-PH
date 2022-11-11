be251f3566daee9bb63de96d8e1b81afd27cefe6
AnonPrixorPH <62997139+AnonPrixorPH@users.noreply.github.com>
2022-11-11 20:01:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-unzip methods.zip; unzip ATLAS-BYPASS.zip; unzip ATLAS-METHODS.zip; mv ATLAS-METHODS methods; mv ATLAS-BYPASS methods; chmod 777 methods/*
+unzip methods.zip; unzip ATLAS-BYPASS.zip; unzip ATLAS-METHODS.zip; mv ATLAS-METHODS methods; mv ATLAS-BYPASS methods; mv proxies.txt methods; chmod 777 methods/*
 pip3 install -r requirements.txt
 sudo apt -y install screen
 python3 atlas.py
